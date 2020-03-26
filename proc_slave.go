48386bb555b02fc333e0cd9a28462ef4c49f2d65
chicknsoup <chicknsoupuds@gmail.com>
2020-03-26 11:41:59 +0700

    Extracted watchParent() to a separated file

--- proc_slave.go
+++ proc_slave.go
@@ -79,26 +79,6 @@ func (sp *slave) run() error {
 	return nil
 }
 
-func (sp *slave) watchParent() error {
-	sp.masterPid = os.Getppid()
-	proc, err := os.FindProcess(sp.masterPid)
-	if err != nil {
-		return fmt.Errorf("master process: %s", err)
-	}
-	sp.masterProc = proc
-	go func() {
-		//send signal 0 to master process forever
-		for {
-			//should not error as long as the process is alive
-			if err := sp.masterProc.Signal(syscall.Signal(0)); err != nil {
-				os.Exit(1)
-			}
-			time.Sleep(2 * time.Second)
-		}
-	}()
-	return nil
-}
-
 func (sp *slave) initFileDescriptors() error {
 	//inspect file descriptors
 	numFDs, err := strconv.Atoi(os.Getenv(envNumFDs))
