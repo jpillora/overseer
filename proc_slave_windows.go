3efe3e188924c0f82091c6fc7979995d08d00335
chicknsoup <chicknsoupuds@gmail.com>
2020-03-28 21:37:57 +0700

    Windows watchParent() fix (#42)
    * Create proc_slave_windows.go
    
    * Extracted watchParent() to a separated file
    
    * Create proc_slave_linux.go
    
    * Update proc_slave_windows.go
    
    Build tags: windows
    
    * Update and rename proc_slave_linux.go to proc_slave_others.go
    
    For other oses than windows
    
    * Update proc_slave_others.go

---
+++ proc_slave_windows.go
@@ -0,0 +1,31 @@
+// +build windows
+
+package overseer
+
+import (
+	"fmt"
+	"github.com/shirou/gopsutil/process"
+	"os"
+	"syscall"
+	"time"
+)
+
+func (sp *slave) watchParent() error {
+	sp.masterPid = os.Getppid()
+	proc, err := os.FindProcess(sp.masterPid)
+	if err != nil {
+		return fmt.Errorf("master process: %s", err)
+	}
+	sp.masterProc = proc
+	go func() {
+		//check process exists
+		for {
+			//should not error as long as the process is alive
+			if _, err := process.GetWin32Proc(int32(sp.masterPid)); err != nil {
+				os.Exit(1)
+			}
+			time.Sleep(2 * time.Second)
+		}
+	}()
+	return nil
+}
